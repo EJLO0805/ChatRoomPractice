50d770cc0726256249c14804259b959f625ecab1
EJ <lyc7485@gmail.com>
2023-01-29 14:16:57 +0800

    Initial Commit

---
+++ ChatRoomPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatRoomPractice
+//
+//  Created by 羅以捷 on 2023/1/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
